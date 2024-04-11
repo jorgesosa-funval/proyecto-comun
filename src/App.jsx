ad8317837badff4acd1452c98a5e38cd176a581f
VianGalop <matcaptu10@gmail.com>
2024-04-11 10:56:41 -0600

    mi aporte

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react"
+import { Input } from "./Vian"
  
  
 
 function App() {
- 
-
   return (
     <>
         <h1>Hola Mundo</h1>
         <h2>Hola Yagual</h2>
+        <Input name="Vianey Galicia" />
     </>
   )
 }
